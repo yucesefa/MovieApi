f6f558ca102978620fadffe2f25a727412d6d812
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-02-04 21:20:52 +0300

    onion arch & entites

---
+++ Core/MovieApi.Domain/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public  class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewComment { get; set; }
+        public int UserRating { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public bool Status { get; set; }
+    }
+}

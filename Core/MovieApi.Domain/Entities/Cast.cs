92f6ad1f3e2b6d850dc1f98d30d14dd8ae60fe5f
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-02-06 14:36:00 +0300

    migration operations & persistence layer & context

---
+++ Core/MovieApi.Domain/Entities/Cast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class Cast
+    {
+        public int CastId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string IamgeUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+    }
+}
---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=YUCESAFA\\SQLEXPRESS;initial Catalog=ApiMovieDb;integrated Security=true;TrustServerCertificate=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}

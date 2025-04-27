f536a777e9ac52c6db381092d0d62ae924e99420
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-27 13:57:08 +0300

    movielist json examples

---
+++ Frontends/MovieApi.Dto/Dtos/MovieDtos/ResultMovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Dto.Dtos.MovieDtos
+{
+    public class ResultMovieDto
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
 
 namespace MovieApi.WebUI.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
         {
             ViewBag.v1 = "Film Listesi";
             ViewBag.v2 = "Ana Sayfa";
             ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7189/api/Movies");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }

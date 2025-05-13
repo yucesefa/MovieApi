5ad7ae584bd2252822d1a3e5a4cbf8c31b411fc7
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-05-13 16:20:31 +0300

    microsoft.ml

---
+++ Frontends/MovieApi.WebUI/ViewComponents/MovieDetailViewComponents/_MovieDetailRelatedMoviesComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.MovieDetailViewComponents
+{
+    public class _MovieDetailRelatedMoviesComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
